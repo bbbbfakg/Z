338d24cce70a1307b5990a25818b0f59b5bcca6e
bbbbfakgziyu <67215136+bbbbfakgziyu@users.noreply.github.com>
2020-06-21 14:48:31 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
 /*注意！本js无任何恶意破坏行为，注入js只是为了好玩，若您认为权益受到侵犯，请通知我，我将删除*/
+<script>
 function startAll(){
   start();
 }
@@ -6,3 +7,4 @@ function startAll(){
   console.log("内部console");
 })();
 console.log("外部console");
+</script>
